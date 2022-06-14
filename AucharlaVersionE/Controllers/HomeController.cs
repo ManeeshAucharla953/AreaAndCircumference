68562eacf316e50f9b848d66d7544e9ae65aa2c8
ManeeshAucharla953 <107507211+ManeeshAucharla953@users.noreply.github.com>
2022-06-14 14:22:05 -0500

    Mid term project

---
+++ AucharlaVersionE/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AucharlaVersionE.Models;
+
+
+//Created by Aucharla Maneesh - 700731564
+namespace AucharlaVersionE.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        //This method is called on first launch which displays Index page
+        public IActionResult Index()
+        {
+            return View(new AucharlaModel());
+        }
+
+        [HttpPost] //Defines the below method is triggered for POST Calls
+
+        //This method is called on click of Submit using Form actionn which displays Results page
+
+        public IActionResult Results(AucharlaModel model) {
+
+            //Check if model state is Valid
+
+            if(ModelState.IsValid)
+            {
+                //Form submission is success and All validations are success
+                return View(model);
+            }
+            //This case is for model state invalid and form is submitted
+            return View("Index",model);
+
+        }
+
+    }
+}
---
+++ AucharlaVersionE/Models/AucharlaModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+//Created by Aucharla Maneesh- 700731564
+namespace AucharlaVersionE.Models
+{
+    public class AucharlaModel
+    {
+        // This property is for input number box
+        [Range(1,100, ErrorMessage ="Please enter a number in the range of 1 and 100.")]
+        [Display(Name ="Please enter Circle Measurements:")]
+        public int Measurement { get; set; }
+
+        //Property for check box
+        [Display(Name = "The number entered is Diameter of the Circle:")]
+        public bool IsDiameter { get; set; }
+
+        //Value to display Area
+        public double Area { get; set; }
+
+        //Value to display Circumference
+        public double Circumference { get; set; }
+
+        //Thsi method calculates Area and Circumference based on provided input either by Radius or Diamtere
+        public string CalculateAreaAndCircumference()
+        {
+            if(IsDiameter) // Since its a boolean we can directly check
+            {
+                //Number entered is diameter
+                Area = Math.PI * ((Measurement / 2) * (Measurement / 2));
+                Circumference = Math.PI * Measurement;
+
+                return $"Area of Circle with diameter: {Measurement} units is {Area} units. Circumference is {Circumference}";
+            }
+            else
+            {
+                //Number entered is Radius
+                Area = Math.PI * Measurement * Measurement;
+                Circumference = 2 * Math.PI * Measurement;
+
+                return $"Area of Circle with radius: {Measurement} units is {Area} units. Circumference is {Circumference}";
+            }
+
+        }
+
+    }
+}
